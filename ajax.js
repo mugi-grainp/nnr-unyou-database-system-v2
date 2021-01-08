31e40ab21daf03d183a830c105c4a8d78cda4d42
mugi-grainp <kawa.jnra415@gmail.com>
2021-01-09 01:35:36 +0900

    Ajax送受信部分実装

--- ajax.js
+++ ajax.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('search').addEventListener('click', function() {
+        const hyphen = /\-/g
+        const sdate = document.getElementById('search_date').value.replace(hyphen, "");
+        const formationNumber = document.getElementById('search_formation_number').value;
+        const trainNumber = document.getElementById('search_train_number').value;
+        const queryObject = {
+            tnum: trainNumber,
+            fnum: formationNumber,
+        };
+
+        const queryString = new URLSearchParams(queryObject);
+        const url = "result.cgi/" + sdate + "?" + queryString;
+
+        fetch(url, {
+            method: "GET",
+        })
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(function(bodytext) {
+            const resultBox = document.getElementById('resultbox');
+            resultBox.textContent = bodytext;
+        });
 
     }, false);
 }, false);
